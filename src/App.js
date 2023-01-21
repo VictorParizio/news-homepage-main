fa28f0663b48b236782fcee35fcf64083a97c4ab
Victor Parizio <victorparizio@hotmail.com>
2023-01-21 16:49:31 -0300

    feat (Footer): Adicionado Footer
    Foi adicionado um rodapé simples de identificação dos projetos do Frontend Mentor

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import New from './component/pages/new'
 import Popular from './component/pages/popular'
 import Trending from './component/pages/trending'
 import Categories from './component/pages/categories'
+import Footer from './component/layout/footer';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/trending" element={<Trending />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/component/layout/footer.js
@@ -0,0 +1,12 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="#">Victor Parizio</a>.
+        </div>
+    )
+}
+
+export default Footer
