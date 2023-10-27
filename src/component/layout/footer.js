9fd62807bb62d40aa8efd02fdab866ecd0546e86
Victor Parizio <victorparizio@hotmail.com>
2023-10-27 12:49:38 -0300

    Adicionada screenshot do projeto

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -2,13 +2,11 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <div class="attribution">
-                Challenge by
-                <a href="https://www.frontendmentor.io/profile/VictorParizio" target="_blank">Frontend Mentor</a>.
-                Coded by <a href="https://www.linkedin.com/in/victorpariziobackend/" target="_blank">Victor Parizio</a>.
-            </div>
-        </footer>
+        <div class="attribution">
+            Challenge by
+            <a href="https://www.frontendmentor.io/profile/VictorParizio" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://www.linkedin.com/in/victorpariziobackend/" target="_blank">Victor Parizio</a>.
+        </div>
     )
 }
 
